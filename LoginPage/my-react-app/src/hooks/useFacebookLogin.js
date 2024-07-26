3c2d951f15899bd56125e0f7f05966732dc71b67
Amrutha Akhila <152167700+AmruthaAkhila0730@users.noreply.github.com>
2024-07-26 17:01:02 +0530

    useFacebookLogin.js

--- LoginPage/my-react-app/src/hooks/useFacebookLogin.js
+++ LoginPage/my-react-app/src/hooks/useFacebookLogin.js
@@ -1,1 +1,37 @@
+import { useEffect } from 'react';
+
+const useFacebookLogin = (appId) => {
+  useEffect(() => {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId: appId,
+        cookie: true,
+        xfbml: true,
+        version: 'v12.0'
+      });
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, [appId]);
+
+  const loginWithFacebook = (callback) => {
+    window.FB.login(response => {
+      if (response.authResponse) {
+        window.FB.api('/me', { fields: 'name,email' }, callback);
+      } else {
+        console.log('User cancelled login or failed.');
+      }
+    }, { scope: 'email' });
+  };
+
+  return { loginWithFacebook };
+};
+
+export default useFacebookLogin;
 
